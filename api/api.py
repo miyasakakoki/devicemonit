503bcc72661fbba131188e7f4a7535db2883f549
miyasaka <mga4ttu@gmail.com>
2016-12-19 12:10:16 +0900

    api簡易実装

--- api/api.py
+++ api/api.py
@@ -1,17 +1,49 @@
 #!/usr/bin/python3
 
 import falcon
+from influxdb import InfluxDBClient
+
+host='localhost'
+port=8086
+user='test'
+password='mytestuser'
+dbname='devicemonit'
+
+#Query for Searching the Device ID from Database
+q = "select sname from Devices where ID = {0};"
+
+
+def existdevice( client, measurement ):
+	ret = client.query( "show measurements with measurement = {0};".format(measurement) )
+	return (len( ret.raw ) > 0)
+def dbwrite( client, measurement, value, tag ):
+	return client.write_points( [{"measurement":measurement,"tags":tag,"time":"","fields":{"value":value}}] )
+	
 
 class MyAPI( object ):
-	def on_get( self, req, res ):
+	def on_get( self, req, res, id ):
+		#Connect to Database
+		cli = InfluxDBClient( host, port, user, password, dbname )
+		if !(existdevice( cli, id )):
+			raise falcon.HTTPNotFound()
+		if not dbwrite( cli, id, 1, {"stat":"up"} ):
+			print("miss1")
 		res.status = falcon.HTTP_200
-		res.body = 'test'
-		res.content_type = 'text/plain'
-	def on_post( self, req, res ):
+		res.body = '{"stat":"ok"}'
+		res.content_type = 'application/javascript'
+	
+	def on_post( self, req, res, id ):
+		cli = InfluxDBClient( host, port, user, password, dbname )
+		if !(existdevice( cli, id )):
+			raise falcon.HTTPNotFound()
+		if not dbwrite( cli, id, 1, {} ):
+			print("miss2")
 		res.status = falcon.HTTP_200
-		res.content_type = 'test/plain'
-		res.body = 'post'
+		res.content_type = 'application/javascript'
+		res.body = '{"stat":"ok"}'
+
+
 app = falcon.API()
-app.add_route( '/', MyAPI() )
+app.add_route( '/{id}', MyAPI() )
 
 
