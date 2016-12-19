40a6108bc6a431e1851559add5844135f49c647d
miyasaka <mga4ttu@gmail.com>
2016-12-19 16:01:00 +0900

    データベースの設計

--- ui/schema.sql
+++ ui/schema.sql
@@ -1,7 +1,21 @@
 drop table if exists User;
 create table User(
-	id integer primary key autoincrement,
-	name text not null
+	uid integer primary key autoincrement, 
+	name text not null,
+	pass text not null
 );
 
-drop table if exists 
+drop table if exists devices;
+create table devices(
+	uid integer,
+	did text not null index,
+	name text not null,
+	description text not null,
+	seq integer ,
+	primary key( uid, did ),
+	foreing key( uid ) references( User ) on delete Cascade
+);
+
+
+
+
