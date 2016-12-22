cf07dd1a25b4f7e64c323634186f925787fc04c4
miyasaka <mga4ttu@gmail.com>
2016-12-23 01:17:35 +0900

    Login機能の実装

--- ui/devmonit.py
+++ ui/devmonit.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-from flask import Flask, request, g
+from flask import Flask, request, g, send_from_directory, render_template, session, redirect, url_for
+from functools import wraps
 import sqlite3
 import os
+import datetime
 
 app = Flask(__name__)
 app.config.from_object( __name__ )
@@ -13,6 +15,7 @@ app.config.update( dict(
 	PASSWORD = 'default'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config['SECRET_KEY'] = os.urandom(24)
 
 def condb():
 	"""Connect to Database"""
@@ -30,6 +33,22 @@ def initdb():
 		db.cursor().executescript(f.read())
 	db.commit()
 
+def checklogin( email, password ):
+	cur = getdb().cursor()
+	cur.execute( "select * from User where email = ? and pass = ?;", (email,password) )
+	ret = cur.fetchone()
+	if ret is None:
+		return {"stat":"None"}
+	return {"stat":"OK", "uid":ret["uid"], "Name":ret["Name"] }
+
+def login_required(f):
+	@wraps(f)
+	def decorated_function( *args, **kwargs ):
+		if "uid" not in session:
+			return redirect( url_for("login") )
+		return f( *args, **kwargs )
+	return decorated_function
+
 @app.cli.command('initdb')
 def initdb_command():
 	"""initialzes  the database"""
@@ -43,9 +62,49 @@ def closedb(error):
 
 
 
+
 @app.route( "/", methods=["GET"] )
+def test():
+	return "OK"
+
+@app.route( "/login", methods=["GET"] )
 def login_page():
-	return loginpage
+	return render_template( 'login.html' )
+
+@app.route( "/login", methods=["POST"] )
+def login():
+	if "uid" in session:
+		return redirect( url_for( "dashboard" ) )
+	f = request.form
+	if "email" in f and len(f["email"]) > 2 and "password" in f and len(f["password"]) > 2:
+		res = checklogin( email=f["email"], password=f["password"] )
+		if res["stat"] == "OK":
+			session["uid"] = res["uid"]
+			session["Name"] = res["Name"]
+			session["Date"] = datetime.datetime.now()
+			return redirect( url_for("dashboard_page") )
+	error = "Invalid credentials.Please try again."
+	return render_template( 'login.html', error=error )
+
+
+@app.route( "/dashboard", methods=["GET"] )
+@login_required
+def dashboard_page():
+	return "UID:" + str(session["uid"]) + "   Name: "+ session["Name"] + "   Date:"+ session["Date"].strftime( "%y%m%d %H:%M:%s" )
+
+@app.route( "/signup", methods=["GET"] )
+def signup_page():
+	return render_template( 'signup.html' )
+
+@app.route( "/signup", methods=["POST"] )
+def signup():
+	t = request.get_json(force=True)
+	return "OK"
+
+
+@app.route('/<path:path>')
+def send_js(path):
+	return send_from_directory('statics', path)
 
 if __name__ == "__main__":
 	app.run()
--- ui/schema.sql
+++ ui/schema.sql
@@ -3,9 +3,12 @@ PRAGMA foreign_keys = ON;
 drop table if exists User;
 create table User(
 	uid integer primary key autoincrement, 
+	email text not null,
 	name text not null,
 	pass text not null
 );
+
+insert into User( email, name, pass ) values( "asdf@asdf", "Test User", "password" );
 
 drop table if exists devices;
 create table devices(
