cf2b0189e09f1c136497731229eee0b4d93e5ccc
miyasaka <mga4ttu@gmail.com>
2017-01-06 16:05:55 +0900

    agentの訂正

--- agent/agent.sh
+++ agent/agent.sh
@@ -9,15 +9,16 @@ fi
 while true; do
 	dat=`echo "{\"seq\":${seq}, \"stat\":\"OK\", \"log\":{\"seq\":${log}}}"`
 	echo $dat
-	if curl -X POST -d ${dat} http://${1}/${2} -sS -m 1; then
+	if curl -X POST -d "${dat}" http://${1}/${2} -sS -m 1 -H "Accept: application/json" -H "Content-type: application/json"; then
 		seq=0
-		rm log
+		rm log 2> /dev/null
 	else
 		seq=$((++seq))
 		if [ ${log} -eq 0 ]; then
 			echo ${seq}>log
 		fi
 	fi
+	sleep 30
 done
 
 
